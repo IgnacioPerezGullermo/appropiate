cb3fb808dae73700a7b05690f5450d10721107c3
El Bebelo <nacho71197@gmail.com>
2023-02-01 19:55:23 -0300

    deploy 2

--- api/appropiate-api/src/main.ts
+++ api/appropiate-api/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
-  await app.listen(parseInt(Number(process.env.PORT), '0.0.0.0') || '3001');
+  await app.listen(process.env.PORT, '0.0.0.0' || '3001');
 }
 bootstrap();
