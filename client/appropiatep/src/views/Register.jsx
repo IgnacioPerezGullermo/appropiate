470b612c5487c9673382676a986fbb70e3a7e48b
Jesu Oliva <olivap.mariajesus@gmail.com>
2023-02-14 17:28:56 -0300

    upgradeFormStart

--- client/appropiatep/src/views/Register.jsx
+++ client/appropiatep/src/views/Register.jsx
@@ -1,15 +1,37 @@
-import {
-
-  FormControl, FormErrorMessage,
-  FormHelperText, FormLabel
-} from '@chakra-ui/react';
+import { Box, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input, List, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from '@chakra-ui/react';
 import React from 'react';
 
-export const Register = () => {
-  return <div>This is the Register page</div>
-  /*<FormControl isRequired>
-  <FormLabel>First name</FormLabel>
-  <Input placeholder='First name' />
-</FormControl>*/
-  ;
+export const Register = (
+  
+) => {
+  return (
+    <Box>
+      <Heading>Completa tu registro</Heading>
+      <List spacing={3}>
+        <FormControl isRequired>
+          <FormLabel> Renta líquida</FormLabel>
+          <Input placeholder='$' />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Edad</FormLabel>
+            <NumberInput max={99} min={18}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+        </FormControl>        
+        <FormControl isRequired>
+          <FormLabel> Ahorro actual</FormLabel>
+          <Input placeholder='$' />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel> Crédito Hipotecario PRE-aprobado</FormLabel>
+          <Input placeholder='$' />
+          <FormHelperText>Escribe el monto</FormHelperText>
+        </FormControl>
+      </List>
+    </Box>
+  );
 }; 
