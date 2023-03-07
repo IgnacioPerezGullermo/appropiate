89df73ed816b91e1c59831552c7c2282f077f567
Jesu Oliva <olivap.mariajesus@gmail.com>
2023-03-07 19:19:21 -0300

    Matchpropierty

---
+++ client/appropiatep/src/components/DisplayPropierty/Components/copiaCars.jsx
@@ -0,0 +1,106 @@
+import { Box, SimpleGrid } from '@chakra-ui/react';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSearchedPropierties } from '../../../redux/properties/propertiesAction';
+import { Pagination } from '../../Pagination/Pagination';
+import { PropiertyCard } from './PropiertyCard';
+import { upUserMatchFilter } from '../../../utils/upUserMatch';
+
+export const PropiertyCards = ({ salary }) => {
+    const { loading, userInfo, error, success } = useSelector(
+    (state) => state.auth
+    );
+    const dispatch = useDispatch();
+  const { propierties } = useSelector((state) => state.propierties);
+  const [Page, setPage] = React.useState(0);
+  const [PageSize, setPageSize] = React.useState(6);
+  let count = 0;
+  React.useEffect(() => {
+    dispatch(
+      getSearchedPropierties({
+        page: Page,
+        pageSize: PageSize,
+        projectname: '',
+      })
+    );
+  }, [Page, PageSize]);
+  if (!userInfo?.clientId) {
+    return (
+      <Box w={'90vw'} ml={'4vw'} mt={'14vh'} h={'container.md'} p={1}>
+        <SimpleGrid columns={'3'} p={0} gap={6} ml={5}>
+          {propierties?.data?.map((prop) => {
+            return (
+              <PropiertyCard
+                key={prop.id}
+                id={prop.id}
+                bedr={prop.bedr}
+                bath={prop.bath}
+                price={prop.price}
+                commune={prop.commune}
+                region={prop.region}
+                /*storage={prop.storage}
+        parking={prop.parking}*/
+                caprate={prop.caprate}
+                //totalarea={prop.totalarea}
+                //deliverytype={prop.deliverytype}
+              />
+            );
+          })}
+        </SimpleGrid>
+        {propierties?.total > PageSize && (
+          <Pagination
+            page={Page}
+            pageSize={PageSize}
+            setPage={setPage}
+            total={propierties?.total}
+          />
+        )}
+      </Box>
+    );
+  } else {
+    return (
+      <Box w={'90vw'} ml={'4vw'} mt={'14vh'} h={'container.sm'} p={1}>
+        <SimpleGrid columns={'3'} p={0} gap={6} ml={5}>
+          {propierties?.data?.map((prop) => {
+            if (
+                upUserMatchFilter(
+                    userInfo?.client.basicIncome,
+                    userInfo?.client.currentSavings, 
+                    userInfo?.client.bankCredit, 
+                    userInfo?.client.age, 
+                    4.3, 
+                    prop.price, 
+                    15
+                ) === true
+            ) {
+              return (
+                <PropiertyCard
+                  key={prop.id}
+                  id={prop.id}
+                  bedr={prop.bedr}
+                  bath={prop.bath}
+                  price={prop.price}
+                  commune={prop.commune}
+                  region={prop.region}
+                  /*storage={prop.storage}
+          parking={prop.parking}*/
+                  caprate={prop.caprate}
+                  //totalarea={prop.totalarea}
+                  //deliverytype={prop.deliverytype}
+                />
+              );
+            }
+          })}
+        </SimpleGrid>
+        {propierties?.total > PageSize && (
+          <Pagination
+            page={Page}
+            pageSize={PageSize}
+            setPage={setPage}
+            total={propierties?.total}
+          />
+        )}
+      </Box>
+    );
+  }
+};
